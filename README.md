e0fc7dfcf00e580d6bf5579ca40d81aba36a50a5
yoieh <yoieh@live.se>
2016-09-27 23:36:02 +0200

    added the skritp and a readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# opencart_selinux_permissions
---
+++ opencart_permissions.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+if [[ "$#" -eq 1 ]]; then
+	inDocRoot=$1
+else
+	printf "You need to input the DocumentRoot of opencart\n"
+	exit 1
+fi
+
+if [[ "${inDocRoot: -1}" == "/" ]]; then
+	docRoot=${inDocRoot::-1}
+else
+	docRoot=${inDocRoot}
+fi
+
+# Owner And Group
+sudo chown -R apache:apache ${docRoot}
+
+# Permissions
+sudo chmod -R 774 ${docRoot}
+sudo chmod -R 774 ${docRoot}/config.php
+sudo chmod -R 774 ${docRoot}/admin/config.php
+sudo chmod -R 774 ${docRoot}/image
+sudo chmod -R 774 ${docRoot}/image/cache
+sudo chmod -R 774 ${docRoot}/image/catalog
+sudo chmod -R 774 ${docRoot}/system/storage/cache
+sudo chmod -R 774 ${docRoot}/system/storage/logs
+sudo chmod -R 774 ${docRoot}/system/storage/download
+sudo chmod -R 774 ${docRoot}/system/storage/upload
+sudo chmod -R 774 ${docRoot}/config.php
+sudo chmod -R 774 ${docRoot}/system/storage/modification
+
+# SeLinux
+sudo chcon -Rv --type=httpd_sys_rw_content_t ${docRoot}/config.php
+sudo chcon -Rv --type=httpd_sys_rw_content_t ${docRoot}/admin/config.php
+sudo chcon -Rv --type=httpd_sys_rw_content_t ${docRoot}/image
+sudo chcon -Rv --type=httpd_sys_rw_content_t ${docRoot}/image/cache
+sudo chcon -Rv --type=httpd_sys_rw_content_t ${docRoot}/image/catalog
+sudo chcon -Rv --type=httpd_sys_rw_content_t ${docRoot}/system/storage/cache
+sudo chcon -Rv --type=httpd_sys_rw_content_t ${docRoot}/system/storage/logs
+sudo chcon -Rv --type=httpd_sys_rw_content_t ${docRoot}/system/storage/download
+sudo chcon -Rv --type=httpd_sys_rw_content_t ${docRoot}/system/storage/upload
+sudo chcon -Rv --type=httpd_sys_rw_content_t ${docRoot}/var/www/opencart/system/storage/modification
+
+exit 0
+
